92a7fb155174549cd1926ac3fb56f728429eca8b
Miary Andria <themia@gmail.com>
2023-01-29 21:10:05 -0800

    Adjusted requirements

--- src/config/common.py
+++ src/config/common.py
@@ -99,6 +99,7 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 
+'''
 # Postgres
 DATABASES = {
     'default': {
@@ -110,6 +111,15 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    } 
+}
+
 
 # General
 APPEND_SLASH = True
